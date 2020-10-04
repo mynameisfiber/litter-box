d3c1f4c9bcb0f05037cd397d2eef6f7e160494d1
Micha Gorelick <mynameisfiber@gmail.com>
2020-10-04 17:14:40 +0200

    Change back to relative change to trigger high res

--- litter-box.ino
+++ litter-box.ino
@@ -11,7 +11,7 @@ static bool CALIBRATE = false;
 static float SCALE = -24;  // Aquired by running calibration mode
 static int N_READINGS = 10;
 static unsigned int TARE_INTERVAL = 50;  // Number of low-res loops before taring
-static double HIGHRES_CHANGE = 100;  // Grams changed in reading to trigger high-res
+static double HIGHRES_CHANGE = 0.25;  // Percent change in reading to trigger high-res
 static double HIGHRES_TIMEOUT = 10;  // Number of seconds to stay in high-res mode
 static byte server[] = { 192, 168, 1, 2 }; 
 static int port = 2003;
@@ -19,6 +19,7 @@ static int port = 2003;
 
 time_t highres_start = 0;
 unsigned int n_lowres = 0;
+float unit_last = 0;
 
 TCPClient client;
 
@@ -88,19 +89,22 @@ void send_long(char* field, long value) {
 
 int calculate_dt(float unit) {
     time_t now = Time.now();
-    
+    float rel_change = 1 - unit / unit_last;
+    unit_last = unit;
+    String message = String(rel_change);
+
     if (highres_start != 0 && now - highres_start <= HIGHRES_TIMEOUT) {
-        Particle.publish("highres-staying");
+        Particle.publish("highres-staying", message.c_str());
         return 500;
-    } else if (unit > HIGHRES_CHANGE) {
-        Particle.publish("highres-entering");
+    } else if (rel_change > HIGHRES_CHANGE && n_lowres >= 1) {
+        Particle.publish("highres-entering", message.c_str());
         highres_start = now;
         n_lowres = 0;
         return 500;
     }
     
     n_lowres += 1;
-    Particle.publish("lowres");
+    Particle.publish("lowres", message.c_str());
     return 10000;
 }
 
